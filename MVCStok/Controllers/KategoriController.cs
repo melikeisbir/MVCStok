a0c7bc953e46939bbf211a6a15e8ce871f849d22
Melike İsbir <melikeisbr@gmail.com>
2023-08-16 23:02:08 +0300

    Add project files.

---
+++ MVCStok/Controllers/KategoriController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Protocols;
+using MVCStok.Models.Entity;
+
+namespace MVCStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MVCDbStokEntities1 db= new MVCDbStokEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+        [HttpGet] // Herhangi bir işlem yapmazsam sayfayı geri döndürme işlemi
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+        [HttpPost] //Sayfaya herhangi bir işlem yapıldığında kaydet butonuna bastığımız zaman işlemi gerçekleştirir.
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if(!ModelState.IsValid) //Modelin durumunda doğrulama işlemi yapılmadıysa return bize yenikategori ekleme vieeini geri göndersim
+            {
+                return View("YeniKategori");
+            }
+
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges(); ;
+            return View();
+
+        }
+        //Silme işlemi uygulamak için
+        public ActionResult SIL(int id)
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", ktgr); //kategori bilgilerinin olduğu viewi döndürsün ktgrden gelecek olan değerlerle
+        }
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID); //pden göndermiş oldugumuz kategorinin idsini bulsun
+            ktg.KATEGORIAD = p1.KATEGORIAD; //ilgili id ye göre ad değiştirecek
+            db.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+    }
+}
---
+++ MVCStok/Controllers/UrunController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStok.Models.Entity;
+
+
+namespace MVCStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MVCDbStokEntities1 db = new MVCDbStokEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLERR.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()  //Kategorileirn içerisinden tblkategorilerin listesini i'ye ata,
+                                                                                   //yeni listeyi seç 
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,   //Bu seçmiş olduğumuz listin değeri i'den geln KATEGORIAD
+                                                 Value = i.KATEGORIID.ToString()  //bunun valuesu da i'nin KATEGORIID'si
+
+                                             }).ToList();
+            ViewBag.dgr = degerler; //sorguları diğer sayfaya değerleri taşımak için
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLERR p1)
+        {
+            //Yeni bir kategori ekleme işlemi-> ürün içerisine kategoriyi dropdowndan seç
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault(); //Katogoriid si p1den gelen tblkategorilerdeki kategoriideye eşit olan değeri getirir.
+            p1.TBLKATEGORILER = ktg;  //p1.kategorilere ktg'den gelen değeri ata
+           
+            db.TBLURUNLERR.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index"); //kaydetme işleminden sonra bizi index sayfasına yönlendirsin.
+        }
+        public ActionResult SIL(int id)
+        {
+            var urun = db.TBLURUNLERR.Find(id); //tblurunler içerisinde id bul 
+            db.TBLURUNLERR.Remove(urun); //urunu kaldır
+            db.SaveChanges();
+            return RedirectToAction("Index"); //Index sayfasına yönlendir
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = db.TBLURUNLERR.Find(id);
+            return View("UrunGetir", urun);
+        }
+
+
+
+    }
+}
